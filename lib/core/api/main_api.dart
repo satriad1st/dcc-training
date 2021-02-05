599b208ef3159d619dbac47f36f412d78a5585f3
Satria Darmawan <satria.darmawan@paxel.co>
2021-02-05 15:33:00 +0700

    main api

--- lib/core/api/main_api.dart
+++ lib/core/api/main_api.dart
@@ -42,4 +42,124 @@ class MainApi {
 
   }
 
+  Future<String> postRequest({
+    String url,
+    Object body,
+    bool useAuth = false,
+    bool isFormData = false
+  }) async {
+
+    var apiToken = await SharedPreferencesHelper.getToken();
+    if(useAuth)
+      this.headers[HttpHeaders.authorizationHeader] = "Bearer $apiToken";
+    try {
+      final response = await dio.post<String>(
+        url,
+        data: isFormData ? body : jsonEncode(body),
+        options: Options(
+          headers: this.headers,
+          validateStatus: (status) => true,
+          contentType: isFormData ? Headers.formUrlEncodedContentType : Headers.jsonContentType
+        )
+      );
+      if(response.statusCode == 200)
+        return jsonEncode(jsonDecode(response.data)['data']);
+      else if(response.statusCode == 400 || response.statusCode == 401)
+        throw jsonDecode(response.data)['message'];
+      else 
+        throw "Failed to load";
+    } catch (error) {
+      throw error;
+    }
+
+  }
+
+  Future<String> patchRequest({
+    String url,
+    Object body,
+    bool useAuth = false,
+    bool isFormData = false
+  }) async {
+
+    var apiToken = await SharedPreferencesHelper.getToken();
+    if(useAuth)
+      this.headers[HttpHeaders.authorizationHeader] = "Bearer $apiToken";
+    try {
+      final response = await dio.patch<String>(
+        url,
+        data: isFormData ? body : jsonEncode(body),
+        options: Options(
+          headers: this.headers,
+          validateStatus: (status) => true,
+          contentType: isFormData ? Headers.formUrlEncodedContentType : Headers.jsonContentType
+        )
+      );
+      if(response.statusCode == 200)
+        return jsonEncode(jsonDecode(response.data)['data']);
+      else if(response.statusCode == 400 || response.statusCode == 401)
+        throw jsonDecode(response.data)['message'];
+      else 
+        throw "Failed to load";
+    } catch (error) {
+      throw error;
+    }
+
+  }
+
+  Future<String> deleteRequest({
+    String url,
+    bool useAuth = false
+  }) async {
+
+    var apiToken = await SharedPreferencesHelper.getToken();
+    if(useAuth)
+      this.headers[HttpHeaders.authorizationHeader] = "Bearer $apiToken";
+    try {
+      final response = await dio.delete<String>(
+        url,
+        options: Options(
+          headers: this.headers,
+          validateStatus: (status) => true
+        )
+      );
+      if(response.statusCode == 200)
+        return jsonEncode(jsonDecode(response.data)['data']);
+      else if(response.statusCode == 400 || response.statusCode == 401)
+        throw jsonDecode(response.data)['message'];
+      else 
+        throw "Failed to load";
+    } catch (error) {
+      throw error;
+    }
+
+  }
+
+  Future<String> putRequest({
+    String url,
+    bool useAuth = false
+  }) async {
+
+    var apiToken = await SharedPreferencesHelper.getToken();
+    if(useAuth)
+      this.headers[HttpHeaders.authorizationHeader] = "Bearer $apiToken";
+    try {
+      final response = await dio.put<String>(
+        url,
+        options: Options(
+          headers: this.headers,
+          validateStatus: (status) => true
+        )
+      );
+      if(response.statusCode == 200)
+        return jsonEncode(jsonDecode(response.data)['data']);
+      else if(response.statusCode == 400 || response.statusCode == 401)
+        throw jsonDecode(response.data)['message'];
+      else 
+        throw "Failed to load";
+    } catch (error) {
+      throw error;
+    }
+
+  }
+
 }
